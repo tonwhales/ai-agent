adb2308a7881f91614d3eec6c2f25ece8954f7d9
Steve Korshakov <steve@korshakov.com>
2021-11-10 18:59:35 +0000

    feat: working test launch

--- test_launch.sh
+++ test_launch.sh
@@ -6,4 +6,4 @@ cd /monad/imperium/software
 ./utility upload /sdcard/ai/ai.bit
 ./utility reset
 cd /sdcard/ai/
-./ai-linux-arm --port /dev/ttyO1 --iterations 20000000 --timeout 60
+./ai-linux-arm --port /dev/ttyO1 --iterations 800000000 --timeout 60
