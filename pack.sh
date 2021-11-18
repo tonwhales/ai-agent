7f1e554a86e21fe706b5a2d78de73cf9b4e3b558
Steve Korshakov <steve@korshakov.com>
2021-11-18 16:19:47 +0000

    feat: use new bit

--- pack.sh
+++ pack.sh
@@ -11,7 +11,7 @@ cd build
 rm -fr package
 mkdir package
 cp ai-linux-arm ./package/
-cp ../bits/i31.bit ./package/ai.bit
+cp ../bits/i32.bit ./package/ai.bit
 cp ../scripts/install.sh ./package/install.sh
 cp ../scripts/start.sh ./package/start.sh
 cd package
