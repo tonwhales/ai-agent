ef795316fa126a61e4b1d090eb838c1cf2b38296
Steve Korshakov <steve@korshakov.com>
2022-04-09 11:58:47 +0100

    feat: r2 tests

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ set -e
 scp -i ../keys/id_rsa_openssh ./build/ai-linux-arm root@192.168.16.120:/monad/ai/ai-linux-arm
 scp -i ../keys/id_rsa_openssh ./bits/test5.bit root@192.168.16.120:/monad/led_ctrl/R2.bit
 scp -i ../keys/id_rsa_openssh ./test_0001.hex root@192.168.16.120:/monad/ai/test_0001.hex
-scp -i ../keys/id_rsa_openssh ./bits/f11.bit root@192.168.16.120:/monad/ai/ai.bit
+scp -i ../keys/id_rsa_openssh ./bits/f13.bit root@192.168.16.120:/monad/ai/ai.bit
 # scp -i ../keys/id_rsa_openssh ./bits/i37.bit root@192.168.16.120:/monad/ai/ai.bit
 scp -i ../keys/id_rsa_openssh ./test_launch.sh root@192.168.16.120:/monad/ai/test_launch.sh
 ssh -i ../keys/id_rsa_openssh root@192.168.16.120 "sh /monad/ai/test_launch.sh"
--- test_r2.sh
+++ test_r2.sh
@@ -9,7 +9,7 @@ set -e
 scp -i ../keys/id_rsa_openssh ./build/ai-linux-arm root@192.168.16.17:/monad/ai/ai-linux-arm
 scp -i ../keys/id_rsa_openssh ./bits/test5.bit root@192.168.16.17:/monad/led_ctrl/R2.bit
 scp -i ../keys/id_rsa_openssh ./test_0001.hex root@192.168.16.17:/monad/ai/test_0001.hex
-scp -i ../keys/id_rsa_openssh ./bits/r2_1.bit root@192.168.16.17:/monad/ai/ai.bit
+scp -i ../keys/id_rsa_openssh ./bits/r2_2.bit root@192.168.16.17:/monad/ai/ai.bit
 # scp -i ../keys/id_rsa_openssh ./bits/i37.bit root@192.168.16.120:/monad/ai/ai.bit
 scp -i ../keys/id_rsa_openssh ./test_launch.sh root@192.168.16.17:/monad/ai/test_launch.sh
 ssh -i ../keys/id_rsa_openssh root@192.168.16.17 "sh /monad/ai/test_launch.sh"
